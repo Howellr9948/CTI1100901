e016b101bb353e7e868283fc0bea1c85d87e249e
Howellr9948 <78620534+Howellr9948@users.noreply.github.com>
2021-02-09 12:25:54 -0500

    Add files via upload
    Intro to Logic and Prog class: M2CW2

---
+++ Output2.java
@@ -0,0 +1,19 @@
+/*
+ * Rheanna Howell
+ * CIS-115-0901
+ * 20210204
+ * M2CW2
+ */
+package output2;
+
+public class Output2 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.print("Programming ");
+        System.out.print("is ");
+        System.out.print("fun.");
+    }
+    
+}
