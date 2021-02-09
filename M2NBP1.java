2f53237bed1a291c9e7bc41659d8acea84197adf
Howellr9948 <78620534+Howellr9948@users.noreply.github.com>
2021-02-09 12:26:37 -0500

    Add files via upload
    Intro to Prog and Logic: M2NBP1

---
+++ M2NBP1.java
@@ -0,0 +1,21 @@
+/*
+ * Rheanna Howell
+ * CIS-115-0901
+ * 20210204
+ * Evaluate understanding of how to create and Submit NetBeans Project
+ */
+package m2nbp1;
+
+public class M2NBP1 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("My name is Rheanna Howell!");
+        System.out.print("I am ");
+        System.out.println("Testing ");
+        System.out.println("how I code.");
+        System.out.println("This is for course CIS-115-0901.");
+        System.out.print("Demo in creating NetBeans Project.");
+    }
+    
+}
