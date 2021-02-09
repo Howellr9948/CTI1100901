1ff4675cd651004b0d4b11e16c868e28d23e3f37
Howellr9948 <78620534+Howellr9948@users.noreply.github.com>
2021-02-09 12:24:43 -0500

    Add files via upload
    Intro and Prog Module Two: M2CW1

---
+++ Output.java
@@ -0,0 +1,19 @@
+/*
+ * Rheanna Howell
+ *CIS-115-0901
+ *20210204
+ *M2CW1
+ */
+package output;
+
+public class Output 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("My major is Computer Science.");
+        System.out.println("I plan to be a software developer.");
+        System.out.println("Programming is fun!");
+    }
+    
+}
