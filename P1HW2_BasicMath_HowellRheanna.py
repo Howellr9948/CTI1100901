8f09c9948b6d24b7ae79d8a86ca2f121caac9756
Rheanna Howell <78620534+Howellr9948@users.noreply.github.com>
2021-02-17 10:29:56 -0500

    Add files via upload
    Basic Math with IDLE Python

---
+++ P1HW2_BasicMath_HowellRheanna.py
@@ -0,0 +1,25 @@
+#Basic Math with Idle Python: Addition and Multiplication
+#20210216
+#CTI-110 P1HW2 - Basic Math
+#Rheanna Howell
+#
+#Pseudocode below:
+#
+#Input first number 
+#Input second number
+#Display first number
+#Display second number
+#
+#Display sum of two variables
+#
+#Display multiplication of two variables
+#
+
+x = int(input())
+y = int(input())
+print(x)
+print(y)
+
+print(x + y)
+
+print(x * y)
