1e1933b7c5c374af966261880345029d9be49356
Rheanna Howell <78620534+Howellr9948@users.noreply.github.com>
2021-02-21 20:41:53 -0500

    Add files via upload
    Distance Traveled

---
+++ P2HW1_DistanceTraveled_RheannaHowell.py
@@ -0,0 +1,14 @@
+#Design a program that calculates the Distance (Speed * Time)
+#20210221
+#CTI - 110 P2HW1 - Distance Traveled
+#Rheanna Howell
+#
+
+speed = int(input())
+time = int(input())
+
+distance = speed * time
+
+print("Speed entered: ", speed)
+print("Time entered: ", time)
+print("Distance entered: ", distance)
